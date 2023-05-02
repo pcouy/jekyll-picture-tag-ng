b0d0e371daa33cdb032aea31875477a0030bb231
pcouy <couy.pierre@gmail.com>
2023-05-02 11:08:21 +0200

    Refactor method order in `OutImageFile`
    Make the responsibility of the class clearer by moving up the more
    important methods

--- lib/jekyll-picture-tag-ng/jekyll/picture_tag/out_image_file.rb
+++ lib/jekyll-picture-tag-ng/jekyll/picture_tag/out_image_file.rb
@@ -17,21 +17,34 @@ module Jekyll
         @collection = nil
       end
 
-      def config
-        @config ||= CONFIG.merge(@site.config["picture_tag_ng"] || {})
+      def copy_file(dest_path)
+        Jekyll.logger.debug "copy_file : #{path} -> #{dest_path} (#{popen_args(dest_path)})"
+        p = IO.popen(popen_args(dest_path))
+        p.close
+        File.utime(self.class.mtimes[path], self.class.mtimes[path], dest_path)
       end
 
-      def replace_args
-        result = config["replace_convert_args"]
-        if picture_versions[@version].is_a?(Hash) &&
-           !picture_versions[@version]["replace_convert_args"].nil?
-          result = picture_versions[@version]["replace_convert_args"]
+      def popen_args(dest_path)
+        args = ["convert", @path]
+        args.concat pre_convert_args
+        args.concat ["-resize", "#{@picture_dim}x>"] unless replace_args
+        if @pictype == "jpg"
+          args.concat ["-background", "##{@config["background_color"]}",
+                       "-flatten", "-alpha", "off"]
         end
-        result
+        args.concat convert_args
+        args.push dest_path
       end
 
-      def picture_versions
-        config["picture_versions"]
+      def destination(dest)
+        output_dir = File.join("img", @version, @dir)
+        output_basename = @site.in_dest_dir(
+          @site.dest,
+          File.join(output_dir, "#{basename}.#{@pictype}")
+        )
+        FileUtils.mkdir_p(File.dirname(output_dir))
+        @destination ||= {}
+        @destination[dest] ||= output_basename
       end
 
       def as_args(input)
@@ -49,6 +62,27 @@ module Jekyll
         end
       end
 
+      def picture?
+        extname =~ /(\.jpg|\.jpeg|\.webp)$/i
+      end
+
+      def config
+        @config ||= CONFIG.merge(@site.config["picture_tag_ng"] || {})
+      end
+
+      def picture_versions
+        config["picture_versions"]
+      end
+
+      def replace_args
+        if picture_versions[@version].is_a?(Hash) &&
+           !picture_versions[@version]["replace_convert_args"].nil?
+          picture_versions[@version]["replace_convert_args"]
+        else
+          config["replace_convert_args"]
+        end
+      end
+
       def convert_args
         @convert_args ||= as_args(config["extra_convert_args"]).concat(
           picture_versions[@version].is_a?(Hash) &&
@@ -61,40 +95,6 @@ module Jekyll
           picture_versions[@version].is_a?(Hash) &&
           picture_versions[@version]["pre_extra_convert_args"] || []
         )
-      end
-
-      def picture?
-        extname =~ /(\.jpg|\.jpeg|\.webp)$/i
-      end
-
-      def destination(dest)
-        output_dir = File.join("img", @version, @dir)
-        output_basename = @site.in_dest_dir(
-          @site.dest,
-          File.join(output_dir, "#{basename}.#{@pictype}")
-        )
-        FileUtils.mkdir_p(File.dirname(output_dir))
-        @destination ||= {}
-        @destination[dest] ||= output_basename
-      end
-
-      def popen_args(dest_path)
-        args = ["convert", @path]
-        args.concat pre_convert_args
-        args.concat ["-resize", "#{@picture_dim}x>"] unless replace_args
-        if @pictype == "jpg"
-          args.concat ["-background", "##{@config["background_color"]}",
-                       "-flatten", "-alpha", "off"]
-        end
-        args.concat convert_args
-        args.push dest_path
-      end
-
-      def copy_file(dest_path)
-        Jekyll.logger.debug "copy_file : #{path} -> #{dest_path} (#{popen_args(dest_path)})"
-        p = IO.popen(popen_args(dest_path))
-        p.close
-        File.utime(self.class.mtimes[path], self.class.mtimes[path], dest_path)
       end
     end
   end
